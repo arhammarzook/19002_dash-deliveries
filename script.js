35b4d297fe0ba971b3b698b080f77b1857a447a6
ArhamMarzook <3692769-ArhamMarzook@users.noreply.replit.com>
2023-08-11 06:09:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript function to navigate to another page
+function navigateToAnotherPage() {
+  // Replace "another_page.html" with the actual path or URL of the destination page
+  window.location.href = "home.html";
+
+}
