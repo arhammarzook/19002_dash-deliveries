e8dea93b271c3acfe3a58705db1c4b80e4688d1c
ArhamMarzook <3692769-ArhamMarzook@users.noreply.replit.com>
2023-09-19 21:48:57 +0000

    format fixing

--- login.js
+++ login.js
@@ -1,36 +1,33 @@
 // This file contains the scripts for handling logins
 
 // This is a different API used for user logins
-//const api_url = "https://retoolapi.dev/AnbI2N/data";
+// const api_url = "https://retoolapi.dev/AnbI2N/data";
 
 // Defining async function
 async function check_login() {
   // Storing response
   const response = await fetch(api_url);
 
-  // Storing data in form of JSON
+  // Storing data in the form of JSON
   var data = await response.json();
 
   // Get the inputted email and password
-  const emailText = document.getElementById("Usertext")
-  const passwordText = document.getElementById("PasswordText")
+  const emailText = document.getElementById("Usertext");
+  const passwordText = document.getElementById("PasswordText");
 
-    
-  data.forEach(function(account){
-    if(emailText.value == account.email && passwordText.value == account.password) {
-      alert("Login successful")
-    
+  data.forEach(function(account) {
+    if (emailText.value === account.email && passwordText.value === account.password) {
+      alert("Login successful");
+
       // Set the session to logged in
-      console.log("Setting session to logged in")
-      sessionStorage.setItem('userStatus', 'loggedIn')
+      console.log("Setting session to logged in");
+      sessionStorage.setItem('userStatus', 'loggedIn');
 
-      location.reload()
+      location.reload();
       // Task: What should happen once the user has logged in? Do they go to a dashboard?
-
     }
-  })
+  });
 }
-
 
 function remove(item_id) {
   return fetch(api_url + "/" + item_id, {
@@ -38,15 +35,5 @@ function remove(item_id) {
     headers: {
       "Content-type": "application/json; charset=UTF-8"
     }
-  })
+  });
 }
-
-
-
-
-
-
-
-
-
-
--- script.js
+++ script.js
@@ -32,3 +32,35 @@ async function initialise_data() {
   }
 }
 
+// This file contains all global scripts that should work on ALL pages of the website
+// This script needs to be added on every HTML page for all the pages to access these scripts
+
+const loginButton = document.getElementsByClassName('login-link');
+const logoutButton = document.getElementsByClassName('logout-link');
+
+console.log("User logged in? " + sessionStorage.getItem('userStatus'))
+
+// Check if the user is logged in. If they are, the Log in button will be replaced with Log out
+if(sessionStorage.getItem('userStatus') == 'loggedIn') {
+    console.log('User is logged in')
+    loginButton[0].style.display = 'none';
+    logoutButton[0].style.display = 'inline-block';
+} else {
+    console.log('User is logged out')
+    loginButton[0].style.display = 'inline-block';
+    logoutButton[0].style.display = 'none';
+}
+
+
+function logout() {
+  let confirmAction = confirm("Are you sure you want to logout?");
+
+  if(confirmAction) {
+    console.log("Logging out..")
+    console.log("Setting session to logged out...")
+    sessionStorage.setItem('userStatus', 'loggedOut')
+  } else {
+    // Do nothing
+  }
+  
+}
