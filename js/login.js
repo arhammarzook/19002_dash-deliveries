6617d24dcc02a1cefbeacaa9c63de5e1a0b7e075
arhammarzook <105829400+arhammarzook@users.noreply.github.com>
2023-11-09 07:33:48 +0000

    placed everthing into folders and made major changes to login requirments

--- js/login.js
+++ js/login.js
@@ -1,9 +1,14 @@
 // This file contains the scripts for handling logins
 
-// This is a different API used for user logins
-// const api_url = "https://retoolapi.dev/AnbI2N/data";
 // Defining async function
 async function check_login() {
+  // login status
+  userLoggedIn = false;
+  
+  // min lengths
+  minUsernameLength = 6;
+  minPasswordLength = 6;
+  
   // Storing response
   const response = await fetch(api_url);
 
@@ -15,22 +20,32 @@ async function check_login() {
   const passwordText = document.getElementById("PasswordText");
 
   // Check if email/password are entered
-  if (emailText.value !== "" && passwordText.value !== "") {
-    data.forEach(function(account) {
-      if (emailText.value === account.email && passwordText.value === account.password) {
-        alert("Login successful");
-  
-        // Set the session to logged in
-        console.log("Setting session to logged in");
-        sessionStorage.setItem('userStatus', 'loggedIn');
-  
+  if (emailText.value.length < minUsernameLength) {
+    alert(`Please enter a username greater than ${minUsernameLength} characters`);
+  } else {
+    if (passwordText.value.length < minPasswordLength) {
+      alert(`Please enter a password greater than ${minPasswordLength} characters`);
+    } else {
+      data.forEach(function(account) {
+        if (emailText.value === account.email && passwordText.value === account.password) {
+          alert("Login successful");
+
+          // Set the session to logged in
+          console.log("Setting session to logged in");
+          sessionStorage.setItem('userStatus', 'loggedIn');
+          userLoggedIn = true;
+
+          location.reload();
+          // Task: What should happen once the user has logged in? Do they go to a dashboard?
+        }
+      });
+      if (!userLoggedIn) {
+        alert("Incorrect Username/Password");
         location.reload();
-        // Task: What should happen once the user has logged in? Do they go to a dashboard?
-      }
-      else {alert("Incorrect Username/Password");
-      }
-    });
+      } 
   }
+  
+
 }
 
 function remove(item_id) {
@@ -41,4 +56,4 @@ function remove(item_id) {
     }
   });
 }
-
+}
